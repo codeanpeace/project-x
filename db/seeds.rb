ecafb8e4ffa84bf9286b5b603768dec811d57fb9
Zara Peisker <zarapeisker@gmail.com>
2013-06-17 18:57:43 -0700

    saved version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+b = la.xpath("//LearningStandardItem").to_a
+
+b.each do |something|
+  c = something.children.to_a
+  code = c[5].children.text
+  s = Standard.create(:standard => code)
+  grades = c[-4].children.to_a
+  grades = grades.select{|x| grades.index(x) % 2 != 0}.map{|x| x.children.text}
+  grades.each do |y|
+    s.grades << Grade.create_or_find_by_name(y)
+  end
+end
