45391edecf3d6a9eacccaacf1a1b69ebe88e3b08
Harrison Tan <harrisontanh@gmail.com>
2013-06-18 18:53:25 -0700

    Adapt code to find math topic id from standard.

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,46 @@ english_standards.each_with_index do |standard, index|
   end
 end
 
+
+
+
+
+math_standards_file = Nokogiri::XML(File.open("db/la.xml"))
+math_standards = math_standards_file.xpath("//LearningStandardItem")
+
+math_standards.each_with_index do |standard, index|
+
+  standard_array = standard.xpath("//StatementCode")[index].children.text.split(".")
+  if standard_array[3] == "K"
+    @topic_id = standard_array[4]
+  elsif standard_array[3].to_i != 0
+    @topic_id = standard_array[4]
+  else
+    @topic_id = standard_array[3]
+  end
+
+  @topic = case @topic_id
+    when "CC"
+      "Counting & Cardinality"
+#add rest of when statements here! :)
+    else
+      puts "No Match!"
+  end
+
+  new_standard_params = {
+    :standard => standard.xpath("//StatementCode")[index].children.text,
+    :subject => "Math",
+    :description => standard.xpath("//Statement")[index].children.text,
+    :topic => @topic
+  }
+
+  puts new_standard_params
+
+  @new_standard = Standard.create(new_standard_params)
+
+  grades = standard.xpath("//GradeLevels")[index].children.to_a
+  grades = grades.select{|x| grades.index(x) % 2 != 0}.map{|x| x.children.text}
+  grades.each do |grade|
+    @new_standard.grades << Grade.find_or_create_by_grade(grade)
+  end
+end
