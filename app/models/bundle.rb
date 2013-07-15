fb78bb17f0ebcc5abea84a0c92291ee45f413e68
Zara Peisker <zarapeisker@gmail.com>
2013-07-14 17:34:51 -0700

    refactored by moving controller logic into models for subject and grade action

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,5 +1,5 @@
 class Bundle < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :source
 
   has_and_belongs_to_many :resources
   has_and_belongs_to_many :standards
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,10 +1,10 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :kind, :url, :name, :source => :mentor_id
+  attr_accessible :kind, :url, :name, :source #=> :mentor_id
 
   has_and_belongs_to_many :bundles
   has_and_belongs_to_many :standards
   belongs_to :user
   has_many :favorites, :as => :favorited
-  has_many :fans, :through => :favorites, :source => :user
+  # has_many :fans, :through => :favorites, :source => :user
 
 end
