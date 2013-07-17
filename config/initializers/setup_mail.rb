9a3346eb604d171d830410b77196776ad9bfd269
harrisontanh@gmail.com <harrisontanh@gmail.com>
2013-07-17 14:37:56 -0700

    Change localhost:3000 to :5000 on setup_mail.rb initializer.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,5 +9,5 @@ ActionMailer::Base.smtp_settings = {
 }
 
 require 'development_mail_interceptor'
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "localhost:5000"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
