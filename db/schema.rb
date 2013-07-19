7f8f464bbe722a5709cb9c99c9e27ab5ec5872f3
Zara Peisker <zarapeisker@gmail.com>
2013-07-18 19:27:23 -0700

    debugged switch

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(:version => 20130718172645) do
   create_table "bundles", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
